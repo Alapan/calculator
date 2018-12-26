79a0c5260ea4eb118ead2703b74b1126e0d4d675
Alapan Mukherjee <alapan.mukherjee@medisapiens.fi>
2018-12-26 23:59:04 +0200

    Remove unwanted this.userInput

--- components/Display.jsx
+++ components/Display.jsx
@@ -3,55 +3,21 @@ import PropTypes from 'prop-types';
 import _ from 'lodash';
 
 export default class Display extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputDisplay: '0',
-      outputDisplay: '',
-      displayResult: false
-    };
-  }
-
-  static isNumericInput(input) {
-    return input.match(/^\d*\.?\d*$/);
-  }
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (nextProps.output !== prevState.outputDisplay) {
-      return {
-        inputDisplay: '0',
-        outputDisplay: nextProps.output,
-        displayResult: true
-      }
-    } else {
-      // If input is number, no space, otherwise 1 space
-      // between characters
-      if (Display.isNumericInput(nextProps.input)) {
-        if (prevState.inputDisplay === '0') {
-          prevState.inputDisplay = '';
-        }
-
-        return {
-          inputDisplay: `${prevState.inputDisplay}${nextProps.input}`,
-          displayResult: false
-        }
-      }
-      return {
-        inputDisplay: `${prevState.inputDisplay} ${nextProps.input} `,
-        displayResult: false
-      }
-    }
-  }
 
   render() {
-    this.props.updateInputOperation(this.state.inputDisplay);
-    const { inputDisplay, outputDisplay, displayResult } = this.state;
-
-    const display = displayResult ? outputDisplay : inputDisplay;
+    if (this.props.input == '') {
+      return (
+        <input
+          type='text'
+          value={this.props.output}
+          readOnly
+        />
+      );
+    }
     return (
       <input
         type="text"
-        value={display}
+        value={this.props.input}
         readOnly
       />
     );
@@ -60,6 +26,5 @@ export default class Display extends React.Component {
 
 Display.propTypes = {
   input: PropTypes.string,
-  updateInputOperation: PropTypes.func.isRequired,
   output: PropTypes.string
 };
